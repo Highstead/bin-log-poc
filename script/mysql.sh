c1ff00f10c3fbcbfe31dd0175f34955541dcb623
Michael Highstead <highstead@gmail.com>
2019-02-04 13:27:25 -0500

    simplify mysql container setup
    - Add support for GTID
    - use standard mysql docker image
    - use command line arguments instead of docker files

--- script/mysql.sh
+++ script/mysql.sh
@@ -4,22 +4,22 @@ set -e
 HOST=${MYSQL_HOST:-localhost}
 
 # MySQL takes time to startup
-until $(mysql --protocol TCP -h $HOST -u root -e "select 1\G" &> /dev/null)
+until $(mysql --protocol TCP -h $HOST -u root -proot -e "select 1\G" &> /dev/null)
 do
   echo "Waiting for database connection..."
   sleep 3
 done
 
-mysql --protocol TCP -h $HOST -u root -e status;
+mysql --protocol TCP -h $HOST -u root -proot -e status;
 
 echo "Creating MySQL user"
-mysql --protocol TCP -h $HOST -u root information_schema < script/mysql/setup-user.sql;
+mysql --protocol TCP -h $HOST -u root -proot information_schema < script/mysql/setup-user.sql;
 
 echo "Creating MySQL schemas"
 for s in script/mysql/schemas/*.sql;
 do
   echo "Loading ${s}"
-  mysql --protocol TCP -h $HOST -u root information_schema < $s;
+  mysql --protocol TCP -h $HOST -u root -proot information_schema < $s;
 done
 
 
--- up.sh
+++ up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose up -d kafka zookeeper mysql
-[[ $(docker-compose ps kafka zookeeper mysql| grep -c 'Up') == 3 ]] 2> /dev/null
+docker-compose up -d kafka zookeeper mysqlmaster mysqlslave
+[[ $(docker-compose ps kafka zookeeper mysql| grep -c 'Up') == 4 ]] 2> /dev/null
 
 script/mysql.sh
